d15561bc68ad49e419a0ebf4af0743daa12e98b1
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-03-15 15:37:33 +0100

    chore: Legal boilerplate

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,5 @@
+import { Text } from '@chakra-ui/layout';
+import { Center } from '@chakra-ui/layout';
 import { Box } from '@chakra-ui/layout';
 import Navbar from './Navbar';
 import Wrapper from './Wrapper';
@@ -7,6 +9,14 @@ const Layout = ({ children }) => {
     <Box>
       <Navbar />
       <Wrapper>{children}</Wrapper>
+      <Wrapper>
+        <Text color="gray.500" fontSize="sm">
+          LoLMatches isn't endorsed by Riot Games and doesn't reflect the views
+          or opinions of Riot Games or anyone officially involved in producing
+          or managing Riot Games properties. Riot Games, and all associated
+          properties are trademarks or registered trademarks of Riot Games, Inc.
+        </Text>
+      </Wrapper>
     </Box>
   );
 };
